c2877204fead6b038c2daacbfd5cb76609045914
Kyle Arthur <kyle@lancreation.net>
2023-07-19 16:25:56 +1000

    First instance of script added.

---
+++ DirectoryCleaner.ps1
@@ -0,0 +1,21 @@
+﻿$directoryPath = "D:\JunkTemp\FinishedDotTorrents"
+
+# Check if the directory exists
+if (Test-Path -Path $directoryPath) {
+    # Get all files within the directory and its subdirectories
+    $files = Get-ChildItem -Path $directoryPath -File -Recurse
+
+    # Loop through each file and delete it
+    foreach ($file in $files) {
+        try {
+            $file.Delete()
+            Write-Host "Deleted file: $($file.FullName)"
+        } catch {
+            Write-Host "Failed to delete file: $($file.FullName)"
+        }
+    }
+
+    Write-Host "Cleanup completed."
+} else {
+    Write-Host "Directory not found: $directoryPath"
+}
